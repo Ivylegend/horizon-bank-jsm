3633d8267a675a32bdd914f83a28e6397bf596b3
Ivylegend <victoryicha2@gmail.com>
2024-05-13 21:26:00 +0100

    adjust the home page cuz of the table and add readme

--- components/RecentTransactions.tsx
+++ components/RecentTransactions.tsx
@@ -1,16 +1,15 @@
-import Link from "next/link";
-import React from "react";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import { BankTabItem } from "./BankTabItem";
-import BankInfo from "./BankInfo";
-import TransactionsTable from "./TransactionsTable";
-import { Pagination } from "./Pagination";
+import Link from 'next/link'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import { BankTabItem } from './BankTabItem'
+import BankInfo from './BankInfo'
+import TransactionsTable from './TransactionsTable'
+import { Pagination } from './Pagination'
 
 const RecentTransactions = ({
   accounts,
   transactions = [],
   appwriteItemId,
-  page,
+  page = 1,
 }: RecentTransactionsProps) => {
   const rowsPerPage = 10;
   const totalPages = Math.ceil(transactions.length / rowsPerPage);
@@ -19,9 +18,8 @@ const RecentTransactions = ({
   const indexOfFirstTransaction = indexOfLastTransaction - rowsPerPage;
 
   const currentTransactions = transactions.slice(
-    indexOfFirstTransaction,
-    indexOfLastTransaction
-  );
+    indexOfFirstTransaction, indexOfLastTransaction
+  )
 
   return (
     <section className="recent-transactions">
@@ -31,11 +29,12 @@ const RecentTransactions = ({
           href={`/transaction-history/?id=${appwriteItemId}`}
           className="view-all-btn"
         >
-          View All
+          View all
         </Link>
       </header>
+
       <Tabs defaultValue={appwriteItemId} className="w-full">
-        <TabsList className="recent-transactions-tablist">
+      <TabsList className="recent-transactions-tablist">
           {accounts.map((account: Account) => (
             <TabsTrigger key={account.id} value={account.appwriteItemId}>
               <BankTabItem
@@ -46,28 +45,32 @@ const RecentTransactions = ({
             </TabsTrigger>
           ))}
         </TabsList>
+
         {accounts.map((account: Account) => (
           <TabsContent
             value={account.appwriteItemId}
             key={account.id}
             className="space-y-4"
           >
-            <BankInfo
+            <BankInfo 
               account={account}
               appwriteItemId={appwriteItemId}
               type="full"
             />
+
             <TransactionsTable transactions={currentTransactions} />
+            
+
             {totalPages > 1 && (
               <div className="my-4 w-full">
-                <Pagination page={page} totalPages={totalPages} />
+                <Pagination totalPages={totalPages} page={page} />
               </div>
             )}
           </TabsContent>
         ))}
       </Tabs>
     </section>
-  );
-};
+  )
+}
 
-export default RecentTransactions;
+export default RecentTransactions
